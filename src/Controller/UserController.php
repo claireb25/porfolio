282c9d3429285dd477f06563290a152d3ead9d54
claireb25 <claire.bourgeois.cb@gmail.com>
2018-06-05 18:26:13 +0200

    ajout formulaire de connexion

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,15 +5,54 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+
 class UserController extends Controller
 {
     /**
      * @Route("/admin", name="admin")
      */
-    public function index()
+    public function admin()
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function connexionForm(Request $request)
+    {
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('send', SubmitType::class, array('label'=>'Send'))
+            ->getForm();
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                // $form->getData() holds the submitted values
+                // but, the original `$task` variable has also been updated
+                $task = $form->getData();
+        
+                // ... perform some action, such as saving the task to the database
+                // for example, if Task is a Doctrine entity, save it!
+                // $entityManager = $this->getDoctrine()->getManager();
+                // $entityManager->persist($task);
+                // $entityManager->flush();
+        
+                return $this->redirectToRoute('admin');
+            }
+
+        return $this->render('user/connexion.html.twig', [
+            "form"=> $form->createView()
         ]);
     }
 }
