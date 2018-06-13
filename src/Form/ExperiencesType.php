cbd65d2533801a34ec91227a3fb51538911a321b
claireb25 <claire.bourgeois.cb@gmail.com>
2018-06-13 09:20:37 +0200

    crud expériences - création automatique du template, controler qui permettent d'ajouter supprimer updater et read les données liées à l'expériences

---
+++ src/Form/ExperiencesType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Experiences;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExperiencesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('exp_title')
+            ->add('exp_description')
+            ->add('date_start')
+            ->add('date_end')
+            ->add('place')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Experiences::class,
+        ]);
+    }
+}
