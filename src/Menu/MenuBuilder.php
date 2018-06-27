c27dd69c7112d1cbdc37475bcc9cbc890089c175
claireb25 <claire.bourgeois.cb@gmail.com>
2018-06-27 09:19:53 +0200

    design du backoffice + formulaires tous OK pour intégration des données

--- src/Menu/MenuBuilder.php
+++ src/Menu/MenuBuilder.php
@@ -19,8 +19,12 @@ class MenuBuilder
     public function createSidebarMenu(RequestStack $requestStack)
     {
         $menu = $this->factory->createItem('root');
-
+       #$presentation = $em->getRepository('App:Presentation')->getId();
         $menu->addChild('Profil', ['route' => 'presentation_index'])->setAttribute('class','onglet');
+        #$menu['Profil']->addChild('Edition',['route' => 'presentation_edit']);
+                                             #'routeParameters' => array('id' => $presentation->getId()) ]);
+        #routeParameters' => ['id' => $blog->getId()]
+        
         $menu->addChild('Expériences', ['route' => 'experiences_index'])->setAttribute('class','onglet');
         $menu->addChild('Formation', ['route' => 'school_index'])->setAttribute('class','onglet');
         $menu->addChild('Compétences', ['route' => 'skills_index'])->setAttribute('class','onglet');
